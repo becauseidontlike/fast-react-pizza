34b763efedfe319299ac35c886ec2f2d940c2be4
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2024-06-05 22:46:48 +0200

    The Box Model: Spacing, Borders, and Display, tailwindcss

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Not-me</div>;
+  return <div className="hidden text-sm font-semibold">Not-me</div>;
 }
 
 export default Username;
