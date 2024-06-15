4807278cc3d88db65e87123a987c4f56f859acbb
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2024-06-15 22:39:12 +0200

    more styling for the menu

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,4 +1,5 @@
 import { formatCurrency } from "../../utilities/helpers";
+import Button from "../../ui/Button";
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
@@ -10,10 +11,12 @@ function MenuItem({ pizza }) {
         alt={name}
         className={`h-24 ${soldOut ? "opacity-70 grayscale" : ""}`}
       />
-      <div className="flex flex-col">
-        <p>{name}</p>
-        <p>{ingredients.join(", ")}</p>
-        <div className="mt-auto">
+      <div className="flex grow flex-col pt-0.5">
+        <p className="font-medium">{name}</p>
+        <p className="text-small capitalize italic text-stone-500">
+          {ingredients.join(", ")}
+        </p>
+        <div className="mt-auto flex items-center justify-between">
           {!soldOut ? (
             <p className="text-sm">{formatCurrency(unitPrice)}</p>
           ) : (
@@ -21,6 +24,7 @@ function MenuItem({ pizza }) {
               Sold out
             </p>
           )}
+          <Button type="small">Add to cart</Button>
         </div>
       </div>
     </li>
--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -81,7 +81,7 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <Button disabled={isSubmitted}>
+          <Button disabled={isSubmitted} type="primary">
             {isSubmitted ? "Placing order..." : "Order now"}
           </Button>
         </div>
