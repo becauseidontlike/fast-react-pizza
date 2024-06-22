8b1f0c5bd7b67da30e25293c32a9d580a29600a6
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2024-06-22 21:34:57 +0200

    deleting cart items on menu level

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -64,3 +64,6 @@ export const getTotalCartPrice = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
 
 //reselect library
+
+export const getCurrentQuantityById = (id) => (state) =>
+  state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
