30da846e1f1dd2cc2bba95c1bdc608b47bc8a1aa
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2024-06-22 22:02:17 +0200

    cart update on menu level

--- src/features/cart/UpdateItemQuantity.jsx
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -2,17 +2,18 @@ import { useDispatch } from "react-redux";
 import Button from "../../ui/Button";
 import { decreaseItemQuantity, increseItemQuantity } from "./cartSlice";
 
-function UpdateItemQuantity({ pizzaId }) {
+function UpdateItemQuantity({ pizzaId, currentQuantity }) {
   const dispatch = useDispatch();
 
   return (
-    <div className="ms:gap-3 flex items-center gap-1">
+    <div className="ms:gap-3 flex items-center gap-2">
       <Button
         type="round"
         onClick={() => dispatch(decreaseItemQuantity(pizzaId))}
       >
         -
       </Button>
+      <span className="text=sm font-semibold">{currentQuantity}</span>
       <Button
         type="round"
         onClick={() => dispatch(increseItemQuantity(pizzaId))}
